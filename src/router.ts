31a076bbc07e51c1faaa4cb10795711546a1246a
uliian <yncxcxz@126.com>
2019-09-09 16:42:38 +0800

    changes

--- src/router.ts
+++ src/router.ts
@@ -7,55 +7,58 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path:"/",
-      redirect:{
-        name :"admin"
+      path: "/",
+      redirect: {
+        name: "admin"
       }
     },
     {
-      path: "/admin/user",
-      component: () => import("./component/user/userManage.vue"),
-      children: [{
-        path: "edit/:userId",
-        component: () => import("./component/user/editUser.vue")
-      }]
+      path: "/admin",
+      name: "admin",
+      component: () => import('./views/AdminBase.vue'),
+      children: [
+        {
+          path: "user",
+          component: () => import("./component/user/userManage.vue"),
+          children: [{
+            path: ":userId",
+            component: () => import("./component/user/editUser.vue")
+          }]
+        },
+        {
+          path: 'role',
+          component: () => import("./component/role/roleManage.vue")
+        },
+        {
+          path: "role/roleResource",
+          component: () => import('./component/role/resource/roleResourceManage.vue')
+        },
+        {
+          path: 'application',
+          component: () => import('./component/application/applicationManage.vue')
+        },
+        {
+          path: 'app/resource',
+          component: () => import('./component/application/resources/appResourceManage.vue')
+        }, {
+          path: 'role/user',
+          component: () => import('./component/role/user/roleUser.vue')
+        }, {
+          path: 'user/resource',
+          component: () => import('./component/user/resource/userResourceManage.vue')
+        }, {
+          path: 'manager',
+          component: () => import('./component/manager/managerUser.vue')
+        }, {
+          path: 'manager/userAuthorization',
+          component: () => import('./component/manager/userAuthorization.vue')
+        },
+      ]
     },
     {
-      path: '/admin/role',
-      component: () => import("./component/role/roleManage.vue")
-    },
-    {
-      path:"/admin/role/roleResource",
-      component:() => import('./component/role/resource/roleResourceManage.vue')
-    },
-    {
-      path:'/admin/application',
-      component:()=> import('./component/application/applicationManage.vue')
-    },
-    {
-      path:'/admin/app/resource',
-      component:()=> import('./component/application/resources/appResourceManage.vue')
-    },{
-      path:'/admin/role/user',
-      component:()=> import('./component/role/user/roleUser.vue')
-    },{
-      path:'/admin/user/resource',
-      component:()=> import('./component/user/resource/userResourceManage.vue')
-    },{
-      path:'/admin/manager',
-      component:()=> import('./component/manager/managerUser.vue')
-    },{
-      path:'/admin/manager/userAuthorization',
-      component:()=> import('./component/manager/userAuthorization.vue')
-    },
-    {
-      path:"/sso",
-      name:"SSO",
-      component:() => import('./views/Sso.vue')
-    },{
-      path:"/admin",
-      name:"admin",
-      component:()=>import('./views/AdminBase.vue')
+      path: "/sso",
+      name: "SSO",
+      component: () => import('./views/Sso.vue')
     }
   ]
 })
