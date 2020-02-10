32ed88fc120f39e62fdec64a33b6bba0261341e0
uliian <yncxcxz@126.com>
2020-02-11 00:14:48 +0800

    changes

--- upload.cmd
+++
@@ -1,3 +0,0 @@
-qshell account drSjyXqBRKIi_Obksj6lKBa-m1fNe2CSHBBdxwqm TX2kqvBRdlIC0zPIAMS4qT2rSKo8__Bsh3V4h7f8 yncxcxz
-qshell qupload ./qiniuconfig_win.cfg
-qshell cdnrefresh -i ./refreshfile.txt
